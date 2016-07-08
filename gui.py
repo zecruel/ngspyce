5d4da0aff2bbeb723eb52bb709dda5dead6d279a
Ezequiel <zecruel@ig.com.br>
2016-07-08 13:50:38 -0300

    Primeiro

---
+++ gui.py
@@ -0,0 +1,245 @@
+# -*- coding: cp1252 -*-
+import Tkinter as tk
+import threading
+import tkFileDialog
+import tkMessageBox
+import ttk
+import subprocess
+import os
+
+#-----------------------------------------------------------------------------------------
+#--------- Constroi a GUI  --------------------------------------
+#------------------------------------------------------------------------------------------
+class Janela(threading.Thread):
+	def __init__(self, group=None, target=None, name=None,
+				args=(), kwargs=None, verbose=None):
+		''' Deriva a classe threading, permitindo a passagem
+		de argumentos na inicializacao da classe '''
+		threading.Thread.__init__(self, group=group, target=target,
+							name=name, verbose=verbose)
+		
+		#os argumentos passados sao armaz. em variaveis internas
+		#args eh uma tupla e kwargs eh um dicionario
+		self.args = args
+		self.kwargs = kwargs
+		
+		#------------- variaveis internas ---------------
+		
+		self.spice = self.args[0] #objeto que controla o NGSpice rodando em segundo plano
+		#------------------------------------------------------------
+		
+		self.start()
+	
+	def sai(self):
+		self.raiz.quit()
+
+	def run(self):
+		''' Modifica o metodo run da classe threading'''
+		
+		# constroi a GUI tk normalmente
+		self.raiz = tk.Tk()
+		self.raiz.protocol("WM_DELETE_WINDOW", self.sai)
+		
+		#------------------------------------------
+		#Cria os botões
+		self.b_vetores = tk.Button(self.raiz, text='Lista Vetores',
+							command=self.ng_vetores)
+		self.b_vetores.grid(row=3, column=0)
+		
+		self.b_ver_vetor = tk.Button(self.raiz, text='Imprime Vetor',
+							command=self.ng_vetor)
+		self.b_ver_vetor.grid(row=5, column=2)
+		
+		self.b_add_vetor = tk.Button(self.raiz, text='->',
+							command=self.add_vet)
+		self.b_add_vetor.grid(row=4, column=2)
+		
+		self.b_sub_vetor = tk.Button(self.raiz, text='<-',
+							command=self.sub_vet)
+		self.b_sub_vetor.grid(row=6, column=2)
+		
+		self.b_plota = tk.Button(self.raiz, text='Plota Vetores',
+							command=self.plotar)
+		self.b_plota.grid(row=3, column=3)
+		
+		self.b_abre_circ = tk.Button(self.raiz, text='Circuito',
+							command=self.ng_abre_circ)
+		self.b_abre_circ.grid(row=1, column=0)
+		
+		self.b_exec = tk.Button(self.raiz, text='Executa',
+							command=self.ng_exec)
+		self.b_exec.grid(row=1, column=1)
+		
+		self.b_para = tk.Button(self.raiz, text='Parada',
+							command=self.ng_para)
+		self.b_para.grid(row=1, column=2)
+		
+		self.b_retoma = tk.Button(self.raiz, text='Retoma',
+							command=self.ng_retoma)
+		self.b_retoma.grid(row=1, column=3)
+		
+		#------------------------------------------
+		#Cria a area de texto de log
+		self.t_log_s = tk.Scrollbar(self.raiz)
+		self.t_log_s.grid(row=7, column=7, rowspan=2, sticky=tk.W+tk.E+tk.N+tk.S)
+		self.t_log = tk.Text(self.raiz, height=10, width=70)
+		self.t_log.grid(row=7, column=0, columnspan=6, rowspan=2)
+		self.t_log_s.config(command=self.t_log.yview)
+		self.t_log.config(yscrollcommand=self.t_log_s.set)
+		self.t_log.after(100, self.temporal) #atualiza a cada 100 ms
+		
+		#------------------------------------------
+		# Caixas de selecao
+		#self.vetor_atual = tk.StringVar()
+		#self.cb_vetores = ttk.Combobox(self.raiz, textvariable=self.vetor_atual)
+		#self.cb_vetores.grid(row=1, column=2)
+		
+		self.cb_plots = ttk.Combobox(self.raiz)
+		self.cb_plots.grid(row=2, column=1)
+		
+		#------------------------------------------
+		# entradas de texto
+		tk.Label(self.raiz, text='Comando:').grid(row=0, column=0, sticky=tk.E)
+		self.e_comando = tk.Entry(self.raiz, width=50)
+		self.e_comando.grid(row=0, column=1, columnspan=2, sticky=tk.W)
+		self.e_comando.delete(0, tk.END)
+		self.e_comando.bind('<Return>', self.ng_comando)
+		
+		self.e_var = tk.Entry(self.raiz)
+		self.e_var.grid(row=2, column=3, sticky=tk.W)
+		
+		self.e_monit = tk.Entry(self.raiz)
+		self.e_monit.grid(row=2, column=4, sticky=tk.W)
+		self.e_monit.delete(0, tk.END)
+		
+
+		
+		#-------------------------------------------
+		# listas
+		self.l_vetores = tk.Listbox(self.raiz, selectmode='multiple')
+		self.l_vetores.grid(row=4, column=0, rowspan=3, sticky=tk.E+tk.N+tk.S)
+		self.l_vetores_s = tk.Scrollbar(self.raiz)
+		self.l_vetores_s.grid(row=4, column=1,rowspan=3, sticky=tk.W+tk.N+tk.S)
+		self.l_vetores_s.config(command=self.l_vetores.yview)
+		self.l_vetores.config(yscrollcommand=self.l_vetores_s.set)
+		
+		self.l_plot = tk.Listbox(self.raiz, selectmode='multiple')
+		self.l_plot.grid(row=4, column=3, rowspan=3, sticky=tk.E+tk.N+tk.S)
+		self.l_plot_s = tk.Scrollbar(self.raiz)
+		self.l_plot_s.grid(row=4, column=4,rowspan=3, sticky=tk.W+tk.N+tk.S)
+		self.l_plot_s.config(command=self.l_plot.yview)
+		self.l_plot.config(yscrollcommand=self.l_plot_s.set)
+		
+		self.raiz.mainloop()	#entra no mainloop da Tk
+	
+	def ng_ver_dir(self):
+		self.spice.cmd('cd')
+
+	def ng_exec(self):
+		self.spice.cmd('bg_run')
+		
+	def ng_para(self):
+		self.spice.cmd('bg_halt')
+		
+	def ng_retoma(self):
+		self.spice.cmd('bg_resume')
+		
+	def ng_vetores(self):
+		self.l_vetores.delete(0,tk.END)
+		nomes = self.spice.vectorNames()
+		for i in nomes:
+			self.l_vetores.insert(tk.END, i)
+		
+	def ng_vetor(self):
+		lista_i = self.l_vetores.curselection()
+		for ind in lista_i:
+			nome = self.l_vetores.get(ind)
+			vet = self.spice.vector(nome)
+			self.t_log.config(state=tk.NORMAL) #permite a escrita
+			for i in vet:
+				self.t_log.insert(tk.END, str(i) + '\n') #insere texto
+			self.t_log.see(tk.END) #rola automaticamente
+			self.t_log.config(state=tk.DISABLED) #somente leitura
+		
+	def ng_comando(self, event):
+		self.spice.cmd(str(self.e_comando.get()))
+		self.e_comando.delete(0, tk.END)
+	
+	def add_vet(self):
+		lista_i = self.l_vetores.curselection()
+		for ind in lista_i:
+			nome = self.l_vetores.get(ind)
+			self.l_plot.insert(tk.END,nome)
+	
+	def sub_vet(self):
+		lista_i = self.l_plot.curselection()
+		for ind in lista_i:
+			self.l_plot.delete(ind)
+			
+	def plotar(self):
+		dir = os.path.dirname(os.path.abspath(__file__)).replace('\\','/') + '/'
+		arq_data = dir + 'plot'
+		arq_gnu = dir + 'gnu_com'
+		lista = self.l_plot.get(0, tk.END)
+		if len(lista) > 0:
+			comm_spice = 'wrdata ' + arq_data
+			for i in lista:
+				comm_spice = comm_spice + ' ' + str(i)
+			self.spice.cmd(comm_spice)
+			
+			p_plot = 'plot '
+			for i in range(len(lista)):
+				p_plot = p_plot + '"' + arq_data + '.data" using '
+				p_plot = p_plot + str(1+i*2) + ':' + str(2+i*2)
+				p_plot = p_plot + ' title "' + str(lista[i]) + '" with lines'
+				if i < (len(lista)-1):
+					p_plot = p_plot + ', \\\n'
+			
+			with open(arq_gnu, 'w') as file_:
+				file_.write(p_plot)
+
+			proc = subprocess.Popen(['E:/documentos/prog/contrade/gnuplot/bin/gnuplot.exe -persist ' + arq_gnu], shell=True,
+					stdin=None, stdout=None, stderr=None, close_fds=True)
+	# -----------------------------------------
+	#Comando de abrir arquivo
+	#------------------------------------------
+	def ng_abre_circ(self):
+		arquivo = tkFileDialog.askopenfilename(title='Abrir')
+		if arquivo:
+			if os.path.exists(arquivo):
+				self.spice.cmd('source ' + str(arquivo))
+			#with open(arquivo) as f:
+				#for num_linha, line in enumerate(f):
+					#pass
+	#------------------------------------------
+	#rotina temporal
+	#--------------------------------------------
+	def temporal(self):
+		self.t_log.config(state=tk.NORMAL) #permite a escrita
+		while not self.spice.log.empty():
+			mens = self.spice.log.get()
+			self.t_log.insert(tk.END, mens + '\n') #insere texto
+			self.t_log.see(tk.END) #rola automaticamente
+		self.t_log.config(state=tk.DISABLED) #somente leitura
+		
+		self.e_monit.delete(0, tk.END)
+		
+		try:
+			vetor = self.spice.vetores[self.e_var.get()]
+		except KeyError:
+			vetor = None
+		
+		try:
+			teste = vetor.creal
+		except AttributeError:
+			vetor = None
+		
+		if vetor:
+			self.e_monit.insert(tk.END, str(vetor.creal))
+		else:
+			self.e_monit.insert(tk.END, '-')
+		
+		self.t_log.after(100, self.temporal) # reagenda
+		
+if __name__ == "__main__":
+	print 'Modulo que constroi a GUI Tk/Tcl da biblioteca do NGSpice'
---
+++ ngspyce.py
@@ -0,0 +1,414 @@
+
+from ctypes import CDLL, CFUNCTYPE, Structure, c_int, c_char_p, c_void_p, \
+        c_bool, c_double, POINTER, c_short, cast
+import re
+import os
+import time
+import Queue
+
+# enum dvec_flags {
+#...
+# };
+class dvec_flags(object):
+   vf_real = (1 << 0)       # The data is real.
+   vf_complex = (1 << 1)    # The data is complex. 
+   vf_accum = (1 << 2)      # writedata should save this vector.
+   vf_plot = (1 << 3)       # writedata should incrementally plot it.
+   vf_print = (1 << 4)      # writedata should print this vector.
+   vf_mingiven = (1 << 5)   # The v_minsignal value is valid.
+   vf_maxgiven = (1 << 6)   # The v_maxsignal value is valid.
+   vf_permanent = (1 << 7)  # Don't garbage collect this vector.
+
+# enum simulation_types {
+#   ...
+# };
+class simulation_type(object):
+    notype = 0
+    time = 1
+    frequency = 2
+    voltage = 3
+    current = 4
+    output_n_dens = 5
+    output_noise = 6
+    input_n_dens = 7
+    input_noise = 8
+    pole = 9
+    zero = 10
+    sparam = 11
+    temp = 12
+    res = 13
+    impedance = 14
+    admittance = 15
+    power = 16
+    phase = 17
+    db = 18
+    capacitance = 19
+    charge = 20
+
+class ngcomplex(Structure):
+    _fields_ = [
+	('cx_real', c_double),
+	('cx_imag', c_double)]
+
+class vector_info(Structure): # acesso a um unico vetor
+    _fields_ = [
+	('v_name', c_char_p), # Same as so_vname
+	('v_type', c_int), # Same as so_vtype
+	('v_flags', c_short), # Flags (a combination of VF_*)
+	('v_realdata', POINTER(c_double)), # Real data
+	('v_compdata', POINTER(ngcomplex)), # Complex data
+	('v_length', c_int)] #Length of the vector
+
+class dvec (Structure): # dados de um vetor de simulacao
+    _fields_ = [
+	('v_name', c_char_p), # Same as so_vname
+	('v_type', c_int), # Same as so_vtype
+	('v_flags', c_short), # Flags (a combination of VF_*)
+	('v_realdata', POINTER(c_double)), # Real data
+	('v_compdata', POINTER(ngcomplex)), # Complex data
+	('v_minsignal', c_double), # Minimum value to plot.
+	('v_maxsignal', c_double), # Maximum value to plot.
+	('v_gridtype', c_int),	# One of GRID_*.
+	('v_plottype', c_int),	#	 One of PLOT_*.
+	('v_length', c_int),	# Length of the vector.
+	('v_rlength', c_int),	# How much space we really have.
+	('v_outindex', c_int),	# Index if writedata is building the vector.
+	('v_linestyle', c_int),	# What line style we are using.
+	('v_color', c_int),	# What color we are using.
+	('v_defcolor', c_char_p), # The name of a color to use.
+	('v_numdims', c_int),	# How many dims -- 0 = scalar (len = 1).
+	('v_dims', c_int * 8),	# The actual size in each dimension. (MAXDIMS=8)
+	('v_plot', c_void_p), # The plot structure (if it has one).
+	('v_nex', c_void_p), # Link for list of plot vectors.
+	('v_link2', c_void_p), # Extra link for things like print.
+	('v_scale', c_void_p)] # If this has a non-standard scale...
+
+class vecinfo(Structure): # acesso a um vetor dentro de um conjunto de simulacoes
+    _fields_ = [
+	('number', c_int), # number of vector, as position in the linked list of vectors, stats with 0 (zero)
+	('name', c_char_p), # name of a actual vector
+	('is_real', c_bool), # TRUE if the actual vector has real data
+	('dvec', POINTER(POINTER(dvec))), # a void pointer to struct dvec *d, the actual vector
+	('dvecscale', POINTER(POINTER(dvec)))] # a void pointer to struct dvec *ds, the scale vector
+
+class vecinfoall(Structure): # acesso a um conjunto de simulacoes
+    _fields_ = [
+	# the plot
+	('name', c_char_p),
+	('title', c_char_p),
+	('date', c_char_p),
+	('type', c_char_p),
+	('veccount', c_int),
+	# the data as an array of vecinfo with length equal to the number of vectors in the plot
+	('vecs', POINTER(POINTER(vecinfo)))]
+
+class vecvalues(Structure): # acesso a um unico valor dentro de um vetor especifico
+    _fields_ = [
+	('name', c_char_p), # name of a specific vector
+	('creal', c_double), # actual data value
+	('cimag', c_double), # actual data value 
+	('is_scale', c_bool), # if 'name' is the scale vector
+	('is_complex', c_bool)] # if the data are complex numbers 
+
+class vecvaluesall(Structure): # acesso a  uma colecao de vetores
+    _fields_ = [
+	('veccount', c_int), # number of vectors in plot
+	('vecindex', c_int), #  index of actual set of vectors. i.e. the number of accepted data points
+	('vecsa', POINTER(POINTER(vecvalues)))] # values of actual set of vectors, indexed from 0 to (veccount - 1)
+
+class app:
+	
+	#globais
+	log = Queue.Queue() #o log vai para uma pilha queue
+	i_vetores ={} #dicionario que lista o nome e o indice de cada vetor disponivel
+	vetores ={} #dicionario que lista o nome  e a estrutura vecvalues de cada vetor disponivel
+	
+	def __init__(self):
+		#------------------------------------------------------------------------------------
+		# inicializacao
+		
+		# Carrega  o dll
+		dir_spice = os.path.dirname(os.path.abspath(__file__)) .replace('\\','/') + '/'
+		#spice_lib_dir = dir_spice + 'share/ngspice/'
+		if os.name == 'posix':			
+			dir_dll = dir_spice + 'lib/'
+			ngspice_dll = 'libngspice.so.0.0.0'
+		else:
+			dir_dll = dir_spice + 'bin/'
+			ngspice_dll = 'libngspice-0.dll'
+		path_dll = dir_dll + ngspice_dll
+		if not os.path.exists(path_dll):
+		    #Give up if none of the above succeeded:
+		    raise Exception('Could not locate ' + path_dll)
+		curr_dir_before = os.getcwd()
+		os.chdir(dir_dll)
+
+		#os.putenv('SPICE_LIB_DIR',  spice_lib_dir)
+		#os.environ['SPICE_LIB_DIR'] =  spice_lib_dir
+		#os.environ['NGSPICE_INPUT_DIR'] =  spice_lib_dir
+		if os.name == 'posix':
+			self.spice = CDLL(path_dll)
+		else:
+			self.spice = CDLL(ngspice_dll)
+		os.chdir(curr_dir_before)
+		
+		# inicializa o NGspice
+		self.spice.ngSpice_Init(self.printfcn, self.statfcn, self.spice_exit, self.data_rt, self.init_rt, None, None)
+		
+		# configura o regex
+		self.end_regex = re.compile('.end', flags = re.IGNORECASE)
+		
+		#-------------------------------------------------------------------------------------
+		# definicao da funcoes a serem chamadas
+		
+		#argtypes traduz o tipo esperado de parametros a serem passados
+		#restype traduz o tipo a ser retornado
+		
+		# int  ngSpice_Command(char* command);
+		self.spice.ngSpice_Command.argtypes = [c_char_p] # envia um comando para o spice
+		
+		# int ngSpice_Circ(char**)
+		self.spice.ngSpice_Circ.argtypes = [POINTER(c_char_p)]
+		
+		#bool ngSpice_running(void)
+		self.spice.ngSpice_running.restype = c_bool
+		
+		#
+		self.spice.ngSpice_AllPlots.restype = POINTER(c_char_p)
+		
+		# /* get info about a vector */
+		# pvector_info ngGet_Vec_Info(char* vecname);
+		self.spice.ngGet_Vec_Info.restype = POINTER(vector_info)
+		self.spice.ngGet_Vec_Info.argtypes = [c_char_p]
+		
+		self.spice.ngSpice_AllVecs.argtypes = [c_char_p]
+		self.spice.ngSpice_AllVecs.restype = POINTER(c_char_p)
+		self.spice.ngSpice_CurPlot.restype = c_char_p
+	# ------------------------------------------------------------------------------------
+	# Estruturas do NGSpice
+
+	# Unit names for use with pint or other unit libraries
+	vector_type = [
+	    'dimensionless',      # notype = 0
+	    'second',             # time = 1
+	    'hertz',              # frequency = 2
+	    'volt',               # voltage = 3
+	    'ampere',             # current = 4
+	    'NotImplemented',     # output_n_dens = 5
+	    'NotImplemented',     # output_noise = 6
+	    'NotImplemented',     # input_n_dens = 7
+	    'NotImplemented',     # input_noise = 8
+	    'NotImplemented',     # pole = 9
+	    'NotImplemented',     # zero = 10
+	    'NotImplemented',     # sparam = 11
+	    'NotImplemented',     # temp = 12
+	    'ohm',                # res = 13
+	    'ohm',                # impedance = 14
+	    'siemens',            # admittance = 15
+	    'watt',               # power = 16
+	    'dimensionless'       # phase = 17
+	    'NotImplemented',     # db = 18
+	    'farad'               # capacitance = 19
+	    'coulomb'             # charge = 21           
+	]
+
+	# ------------------------------------------------------------------------------------
+	# Funcoes de retorno (callbaks)
+	
+	@staticmethod
+	@CFUNCTYPE(c_int, c_char_p, c_int, c_void_p)
+	def printfcn(output, id, ret):
+	    """Callback for libngspice to print a message"""	    
+	    if output.startswith(b'stderr'):
+		app.log.put('ALERTA: '+ output[7:].decode('ascii'))
+	    else:
+		app.log.put(output[7:].decode('ascii'))
+	    return 0
+	    
+	@staticmethod
+	@CFUNCTYPE(c_int, c_char_p, c_int, c_void_p)
+	def statfcn(status, id, ret): 
+	    """Callback for libngspice to report the current status"""
+	    app.log.put(status.decode('ascii'))
+	    return 0
+	    
+	@staticmethod
+	@CFUNCTYPE(c_int, c_int, c_bool, c_bool, c_int, c_void_p)
+	def spice_exit(status, immediate, quit, id, ret): 
+	    """Callback for libngspice to report fatal error in execution"""
+	    app.log.put('ERRO: NGspice saiu inesperadamente. Cod erro ' + str(status))
+	    return 0
+	
+	@staticmethod
+	@CFUNCTYPE(c_int, POINTER(vecvaluesall), c_int, c_int, c_void_p)
+	def data_rt(a_vet, a_vet_len, id, ret): 
+	    """callback function for sending an array of structs containing data values 
+	    of all vectors in the current plot (simulation output) (NULL allowed)"""
+	    
+	    geral = a_vet.contents  #estrutura geral
+	    #abaixo os campos da estrutura geral
+	    num_vet = geral.veccount #numero de vetores
+	    i_atual = geral.vecindex	   # indice do ponto em relacao a simulacao total
+	    lista_vet = geral.vecsa	#lista de ponteiros das estruturas de vetores
+	    #i=0
+	    #vetor = lista_vet[i].contents #acesso a estrutura do vetor indexado por i
+	    # abaixo os campos do vetor atual
+	    #nome = vetor.name
+	    #real = vetor.creal
+	    #imag = vetor.cimag
+	    #escala = vetor.is_scale
+	    #complex = vetor.is_complex
+	    #app.vetores = {}
+	    for i in range(num_vet):
+		    #constroi o dicionario de vetores com nome e indice
+		    vetor = lista_vet[i].contents 
+		    app.vetores[vetor.name] = vetor
+	    
+	    #app.log.put('Vetor ' + str(i) + ': '+ nome +' = '+ str(real) + ' ' + str(imag) + 'i')
+	    
+	    return 0
+	    
+	@staticmethod
+	@CFUNCTYPE(c_int, POINTER(vecinfoall), c_int, c_void_p)
+	def init_rt(vet_info, id, ret):
+	    
+	    geral = vet_info.contents  #estrutura geral
+	    #abaixo os campos da estrutura geral
+	    nome = geral.name
+	    titulo = geral.title
+	    data = geral.date
+	    tipo = geral.type
+	    num_vet = geral.veccount
+	    lista_vet = geral.vecs	#lista de ponteiros das estruturas de info vetores
+	    #i_vetor = lista_vet[i].contents #acesso a estrutura do vetor indexado por i
+	    # abaixo os campos do vetor atual
+	    #i_num = i_vetor.number
+	    #i_nom = i_vetor.name
+	    #i_real = i_vetor.is_real
+	    #i_dvec = i_vetor.dvec
+	    #i_dvecs = i_vetor.dvecscale
+	    app.i_vetores = {}
+	    app.vetores = {}
+	    for i in range(num_vet):
+		    #constroi o dicionario de vetores com nome e indice
+		    i_vetor = lista_vet[i].contents 
+		    app.i_vetores[i_vetor.name] = i_vetor.number
+		    app.vetores[i_vetor.name] = i_vetor.number
+	    #app.log.put('INIT '+nome+titulo+data+tipo)
+	    #app.log.put('Vetor '+ repr(app.i_vetores))
+	    
+	    return 0
+
+	#--------------------------------------------------------------------------------
+
+	def cmd(self, command):
+	    """Send a commang to the ngspice engine"""
+	    self.spice.ngSpice_Command(command.encode('ascii'))
+	    time.sleep(0.1)
+
+	def circ(self, netlist_lines):
+	    """Specify a netlist"""
+	    # Accept an array of lines, or a multi-line string
+	    if issubclass(type(netlist_lines), str):
+		netlist_lines = netlist_lines.split('\n')
+	    # First line is ignored by the engine
+	    netlist_lines.insert(0, '* First line')
+	    # Add netlist end
+	    if not any((end_regex.match(line) for line in netlist_lines)):
+		netlist_lines.append('.end')
+	    netlist_lines = [line.encode('ascii') for line in netlist_lines]
+	    # Add list terminator
+	    netlist_lines.append(None)
+	    array = (c_char_p * len(netlist_lines))(*netlist_lines)
+	    return spice.ngSpice_Circ(array)
+
+	def plots(self):
+	    """List available plots (result sets)"""
+	    ret = []
+	    plotlist = spice.ngSpice_AllPlots()
+	    ii = 0
+	    while True:
+		if not plotlist[ii]:
+		    return ret
+		ret.append(plotlist[ii].decode('ascii'))
+		ii += 1
+
+	def vectorNames(self, plot=None):
+	    """List the vectors present in the specified plot 
+	    
+	    List the voltages, currents, etc present in the specified plot.
+	    Defaults to the last plot.
+	    """
+	    names = []
+	    if plot is None:
+		plot = self.spice.ngSpice_CurPlot()
+	    veclist = self.spice.ngSpice_AllVecs(plot)
+	    ii = 0
+	    while True:
+		if not veclist[ii]:
+		    return names
+		names.append(veclist[ii].decode('ascii'))
+		ii += 1
+
+	def vectors(self, names=None):
+	    """Return a dictionary with the specified vectors
+	    
+	    If names is None, return all available vectors"""
+	    if names is None:
+		plot = spice.ngSpice_CurPlot()
+		names = vectorNames(plot)
+	    return dict(zip(names, map(vector, names)))
+
+	def vector(self, name):
+	    """Return a numpy.ndarray with the specified vector"""
+	    # modificado para retirar a dependencia do numpy
+	    vec = self.spice.ngGet_Vec_Info(name.encode('ascii'))
+	    if not vec:
+		raise RuntimeError('Vector {} not found'.format(name))
+	    vec = vec[0]
+	    if vec.v_length == 0:
+		array = None
+	    elif vec.v_flags & dvec_flags.vf_real:
+		array = []
+		for i in range(vec.v_length):
+			array.append(vec.v_realdata[i])
+	    elif vec.v_flags & dvec_flags.vf_complex:
+		array = []
+		for i in range(vec.v_length):
+			array.append(vec.v_compdata[i])
+	    else:
+		raise RuntimeError('No valid data in vector')
+	    app.log.put('Fetched vector {} type {}'.format(name, vec.v_type))
+	    return array
+    
+if __name__ == "__main__":
+    import gui
+    print 'Modulo de interface ao simulador de circuitos NGSpice.'
+    ''' o codigo abaixo eh somente para teste '''
+    spice = app()
+    janela = gui.Janela(args=(spice,))
+    
+    curr_dir_before = os.path.dirname(os.path.abspath(__file__)).replace('\\','/')
+    dir_cm = os.path.dirname(os.path.abspath(__file__)).replace('\\','/') + '/lib/ngspice/'
+    spice.cmd('cd ' + dir_cm)
+    spice.cmd('codemodel ./analog.cm')
+    spice.cmd('codemodel ./digital.cm')
+    spice.cmd('codemodel ./spice2poly.cm')
+    spice.cmd('codemodel ./xtradev.cm')
+    spice.cmd('codemodel ./xtraevt.cm')
+    spice.cmd('cd ' + curr_dir_before + '/teste_cm')
+    
+    spice.cmd('codemodel ./teste.cm')
+    '''spice.cmd('source ./teste_cm3.cir')
+    spice.cmd('bg_run')
+    
+    while spice.spice.ngSpice_running():
+        pass
+    a = spice.vectorNames()
+    print len(a)
+    for i in range(len(a)):
+	    print a[i]
+    t= spice.vector('time')
+    print len(t)
+    spice.cmd('wrdata teste v(3) v(2)')
+    '''
